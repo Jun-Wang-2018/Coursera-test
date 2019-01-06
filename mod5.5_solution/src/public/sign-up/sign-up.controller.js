4a0068a0f9ef1643a20452d326a35ba2156890a1
Jun-Wang-2018 <2438250677@qq.com>
2019-01-06 17:12:00 +0800

    Assignment5.5

--- mod5.5_solution/src/public/sign-up/sign-up.controller.js
+++ mod5.5_solution/src/public/sign-up/sign-up.controller.js
@@ -4,12 +4,17 @@
 angular.module('public')
 .controller('SignUpController', SignUpController);
 
-SignUpController.$inject = ['SignUpService'];
-function SignUpController(SignUpService) {
+SignUpController.$inject = ['SignUpService', '$filter'];
+function SignUpController(SignUpService, $filter) {
   var signUpCtrl = this;
 
   signUpCtrl.signedUser = SignUpService.signedUser();
   signUpCtrl.addUser = SignUpService.addUser();
+
+  signUpCtrl.capitalize = function (text) {
+    var capitalized_text = $filter('uppercase')(text);
+    return capitalized_text;
+  };
 
   signUpCtrl.submit = function () {
     signUpCtrl.completed = true;
